b134aa13f980041072920154125e98163a5e49c1
Gabriel Job <gabrielpjob2002@gmail.com>
2025-03-10 21:28:24 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,17 @@
-// import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  // const [message, setMessage] = useState('');
+  const [message, setMessage] = useState('');
 
-  // useEffect(() => {
-  //   fetch('http://localhost:5000')
-  //     .then((response) => response.text())
-  //     .then((data) => setMessage(data));
-  // }, []);
+  useEffect(() => {
+    fetch('http://localhost:5000')
+      .then((response) => response.text())
+      .then((data) => setMessage(data));
+  }, []);
 
   return (
     <div>
-      <h1>TESTES1</h1>
+      <h1>{message}</h1>
     </div>
   );
 }
